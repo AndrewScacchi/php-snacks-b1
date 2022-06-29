7523197d4f19b2fe724603289a9334d7c2d08b02
andrea scacchi <andrea.scacchi@live.com>
2022-06-29 10:19:41 +0200

    initial settings

---
+++ snack01-basket.php
@@ -0,0 +1,22 @@
+<!--
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+-->
+
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack01 - Basket</title>
+</head>
+<body>
+    
+</body>
+</html>
